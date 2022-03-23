0bc365c82e0419276d44c848aa44da7df750ef1b
Andrey <maoltin@gmail.com>
2022-03-23 12:26:23 +0300

    feat: lession 2

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import "./index.html";
 
+import "./css/normalize.css";
+import "./css/default.scss";
 import "./css/style.scss";
 
 import "./js/main.ts";
--- src/js/main.ts
+++ src/js/main.ts
@@ -5,3 +5,11 @@ for (let i = 0; i < 5; i++) {
   const newCard = card.cloneNode(true);
   newsList.appendChild(newCard);
 }
+
+const populateText = (): void => {
+  const link = document.querySelectorAll('.news-link')[1];
+  const description = document.querySelectorAll('.news-description')[1];
+  link.textContent = link.textContent + link.textContent + link.textContent;
+  description.textContent = description.textContent + description.textContent + description.textContent;
+};
+populateText();
